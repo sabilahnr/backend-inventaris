712d2a71cbd0f8ee055d67ef54adab79c3db616f
rozyfatmala <rozyfatmala771@gmail.com>
2023-06-14 12:44:05 +0700

    museum

--- routes/api.php
+++ routes/api.php
@@ -40,4 +40,3 @@ Route::post('/add-kualifikasi', [KualifikasiController::class, 'store_kualifikas
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 return $request->user();
 });
-Route::post('/login', [AuthController::class, 'login']); 
